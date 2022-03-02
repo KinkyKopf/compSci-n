0aa16780452d5748f256c0cab3c8053324ec4ad5
KinkyKopf <95887857+KinkyKopf@users.noreply.github.com>
2022-03-02 14:27:22 -0500

    Add files via upload

--- Fraction.java
+++ Fraction.java
@@ -17,7 +17,7 @@ public class Fraction
 		denom=d;
 		simplify();
 	}
-	public Fraction( double d) throws InterruptedException
+	public Fraction( double d) 
 	{
 		num=(int)(d*100000000);
 		denom=100000000;
@@ -40,7 +40,7 @@ public class Fraction
 //			System.out.println(num%i==0&&denom%temp==0);
 			if(num%i==0&&denom%i==0)
 			{
-				System.out.println("True");
+//				System.out.println("True");
 				num=num/i;
 				denom/=i;
 			}
@@ -62,12 +62,56 @@ public class Fraction
 	}
 	public Fraction add(Fraction f)
 	{
-		int tempN=f.getNumerator();
-		if(f.getDenominator()==denom)
-			return(new Fraction(f.getNumerator()+num,denom));
+		int tempN1=f.getNumerator()*denom;
+		int tempD1=f.getDenominator()*denom;
+		int tempD2=denom*f.getDenominator();
+		int tempN2=num*f.getDenominator();
+		
+		return(new Fraction(tempN2+tempN1,tempD2));
+	}
+	public Fraction subtract(Fraction f)
+	{
+	int tempN1=f.getNumerator()*denom;
+	int tempD1=f.getDenominator()*denom;
+	int tempD2=denom*f.getDenominator();
+	int tempN2=num*f.getDenominator();
+		
+		return(new Fraction(tempN1-tempN2,tempD2));
+	}
+	public Fraction multiply(Fraction f)
+	{
+		return new Fraction(f.getNumerator()*num,f.getDenominator()*denom);
+	}
+	public Fraction divide(Fraction f)
+	{
+		return new Fraction(f.getNumerator()*denom,f.getDenominator()*num);
+	}
+	public int compareTo(Fraction f)
+	{
+		int tempN1=f.getNumerator()*denom;
+		int tempN2=num*f.getDenominator();
+		
+		if(tempN1==tempN2)
+		{
+			return 0;
+		}
+		else if(tempN1<tempN2)
+		{
+			return 1;
+		}
 		else
 		{
-			f.getNumerator()
+			return -1;
 		}
 	}
+	public boolean equals(Fraction f)
+	{
+		return(compareTo(f)==0);
+	}
+	public boolean equals(double dd)
+	{
+		return equals(new Fraction(dd));
+	}
+	
+
 }
--- FractionRunner.java
+++ FractionRunner.java
@@ -5,10 +5,16 @@ public class FractionRunner
 
 	public static void main(String[] args) throws InterruptedException 
 	{
-		Fraction uno=new Fraction(20,10);
-		Fraction dos = new Fraction(.87);
+		Fraction uno=new Fraction(1,4);
+		Fraction dos = new Fraction(.5);
 		System.out.println("Uno es: "+ uno);
 		System.out.println("Dos es: "+ dos);
+		
+		System.out.println(uno.add(dos));
+		System.out.println(uno.multiply(dos));
+		System.out.println(uno.subtract(dos));
+		System.out.println(uno.divide(dos));
+		System.out.println(uno.compareTo(dos));
 
 	}
 

096609e1c3b41829be318684c09b3715e0775d81
KinkyKopf <95887857+KinkyKopf@users.noreply.github.com>
2022-03-31 22:44:07 -0400

    Add files via upload

--- FracMenu.java
+++ FracMenu.java
@@ -1,5 +1,6 @@
 package unit5;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FracMenu {
@@ -9,6 +10,7 @@ public class FracMenu {
 		Scanner input =new Scanner(System.in);
 		int selection;
 		boolean exit=false;
+		ArrayList<Fraction> fracArray = new ArrayList<Fraction>();
 		System.out.println("Welcome to my thing please enter what you would like to do!");
 		do
 		{
@@ -27,6 +29,26 @@ public class FracMenu {
 				//add fraction
 				break;
 			case 3:
+				System.out.print("Enter the location where you would like to enter the fraction: ");
+				try
+				{
+					fracArray.add(input.nextInt(),new Fraction(.5));
+					fracArray.remove(input.nextInt());
+					input.nextLine();
+				}catch(  java.lang.IndexOutOfBoundsException e)
+				{
+					System.out.println("You may only insert fractions between the locations 1 and "+fracArray.size()+". Please try again");
+				}
+				
+				//temp block
+				{
+					int num,denom;
+					System.out.print("Enter the numerator of the new fraction: ");
+					num=input.nextInt();
+					System.out.print("Enter the denominator of the new fraction: ");
+					denom=input.nextInt();
+					fracArray.add(input.nextInt(),new Fraction(.5));
+				}
 				//insert a fraction 
 				break;
 			case 4:
@@ -43,7 +65,7 @@ public class FracMenu {
 			}
 			
 		}
-		while(exit);
+		while(!exit);
 	}
 
 }
